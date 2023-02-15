1577c88d6eb5e5eed50bbd722b88b77d950e8581
Hussein <aisackhussein@gmail.com>
2023-02-15 12:29:52 +0300

    Created folder and added the pages, added the routes

--- houzez/src/App.js
+++ houzez/src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import SignIn from "./pages/signIn";
+import Profile from "./pages/profile";
+import SignUp from "./pages/signUp";
+import ForgotPassword from "./pages/forgotPassword";
+import Offers from "./pages/offers";
+
 function App() {
   return (
     <>
-      <h1 className="text-2xl bg-red-400">Heading 3</h1>
-      <p>Hello world</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/sign-up" element={<SignUp />}></Route>
+          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route path="/offers" element={<Offers />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
