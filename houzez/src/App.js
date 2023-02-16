654aa46087c7a459b514148c1fdbbe198996d546
Hussein <aisackhussein@gmail.com>
2023-02-16 14:57:54 +0300

    Added hooks

--- houzez/src/App.js
+++ houzez/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/register";
 import Profile from "./pages/profile";
 import ForgotPassword from "./pages/forgotPassword";
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/profile" element={<Profile />}></Route>
-          <Route path="/login" element={<Login />}></Route>
+          <Route path="/Login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
           <Route path="/offers" element={<Offers />}></Route>
