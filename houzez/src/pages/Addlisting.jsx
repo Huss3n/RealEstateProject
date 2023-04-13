d5682b30ca062e0026f210a24906791061964792
Hussein <aisackhussein@gmail.com>
2023-04-13 19:24:59 +0300

    Removed latlong from form data

--- houzez/src/pages/Addlisting.jsx
+++ houzez/src/pages/Addlisting.jsx
@@ -6,12 +6,16 @@ import { collection, serverTimestamp, addDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { v4 as uuidv4 } from "uuid";
 import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function Addlisting() {
   // get auth
   const auth = getAuth();
   // geolocation hook
   const [geolacationEnabled, setGeolocationEnabled] = useState(true);
+
+  // navigate hook
+  const navigate = useNavigate();
 
   // loading hook
   const [loading, setLoading] = useState(false);
@@ -27,13 +31,11 @@ export default function Addlisting() {
     offer: false,
     regularPrice: 1,
     discountedPrice: 1,
-    latitude: 0,
-    longitude: 0,
     images: {},
   });
 
   // destructuring the form data
-  const { type, propertyName, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice, latitude, longitude, images } = formData;
+  const { type, propertyName, bedrooms, bathrooms, parking, furnished, address, description, offer, regularPrice, discountedPrice, images } = formData;
 
   // on change tracking function
   function onChange(e) {
@@ -70,7 +72,7 @@ export default function Addlisting() {
 
     // check conditions before submittting
     // check the regular and discounted price
-    if (discountedPrice >= regularPrice) {
+    if (+discountedPrice >= +regularPrice) {
       setLoading(false); // prevent loading
       toast.error("Regular price cannot be less than discounted price");
       return; // stop execution
@@ -88,34 +90,42 @@ export default function Addlisting() {
 
     if (geolacationEnabled) {
       const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&components=country:KE&key=${process.env.REACT_APP_GEOCODE_API_KEY}`);
+
       const data = await response.json();
-      // console.log("data:", data);
+      console.log("data:", data);
       // console.log("results:", data.results);
       // console.log(data);
 
+      // check for valid user address
       if (data.status !== "OK" || data.results.length === 0 || data.results[0].partial_match === true) {
         setLoading(false);
-        toast.error("Please enter a correct address");
+        toast.error("Please enter a valid address");
         return;
       }
 
       geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      console.log(`geolat: ${geolocation.lat}`);
       geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+      console.log(`geolng: ${geolocation.lng}`);
 
       // setting the location
-      // location = data.status === "OK" && undefined;
       location = data.results[0]?.formatted_address ?? undefined;
+      // location = data.results[0]?.formatted_address; //this prints locs name
+      console.log(location);
       // console.log(typeof location);
 
-      if (location === undefined || location.includes("undefined")) {
-        setLoading(false);
-        toast.error("Please enter a correct address");
-        return;
-      } else {
-        geolocation.lat = latitude;
-        geolocation.lng = longitude;
-      }
+      // if (location === undefined) {
+      //   setLoading(false);
+      //   toast.error("Please enter a correct address");
+      //   return;
+      // } else {
+      //   geolocation.lat = latitude;
+      //   console.log(latitude);
+      //   geolocation.lng = longitude;
+      //   console.log(longitude);
+      // }
     }
+
     async function storeImage(image) {
       return new Promise((resolve, reject) => {
         const storage = getStorage();
@@ -137,6 +147,8 @@ export default function Addlisting() {
               case "running":
                 console.log("Upload is running");
                 break;
+              default:
+                console.log("An error occured");
             }
           },
           (error) => {
@@ -168,6 +180,10 @@ export default function Addlisting() {
     delete formDataCopy.images;
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
     const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+    delete formDataCopy.latitude;
+    delete formDataCopy.longitude;
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
+    // Navigate()
     setLoading(false);
     toast.success("New listing created");
   }
@@ -324,37 +340,6 @@ export default function Addlisting() {
           placeholder="Enter property address"
           className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded-xl transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-2 active:border-slate-600"
         />
-
-        {/* co-ordinates section for manual input */}
-        {!geolacationEnabled && (
-          <div className="flex space-x-6 mb-6">
-            <div className="">
-              {/* manual latitude input   */}
-              <p className="text-lg font-semibold">Latitude</p>
-              <input
-                type="number"
-                id="latitude"
-                value={latitude}
-                onChange={onChange}
-                required
-                className="rounded-xl text-center w-full px-4 py-2 text-lg text-gray-700 bg-white border border-gray-300 transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600"
-              />
-            </div>
-
-            {/* manual longitude input  */}
-            <div className="">
-              <p className="text-lg font-semibold">Longitude</p>
-              <input
-                type="number"
-                id="longitude"
-                value={longitude}
-                onChange={onChange}
-                required
-                className="rounded-xl text-center w-full px-4 py-2 text-lg text-gray-700 bg-white border border-gray-300 transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600"
-              />
-            </div>
-          </div>
-        )}
 
         {/* description section  */}
         <p className="text-lg font-semibold mb-2">Description</p>
