8f8f28483d0db9d88c2c640a6ea7035032a1cc25
Hussein <aisackhussein@gmail.com>
2023-05-29 10:32:08 +0300

    Featured page edit

--- houzez/src/components/Featured.jsx
+++ houzez/src/components/Featured.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 // import axios from "axios";
 // import PropertyCard from "../../components/Card/propertyCard";
 import { Link } from "react-router-dom";
+import "../index.css";
 
 export default function FeaturedProperty() {
   //   const [user, setUser] = useState({
@@ -56,16 +57,71 @@ export default function FeaturedProperty() {
 
   return (
     <>
-      <div className="max-w-[1440px] mx-auto lg:p-20 pb-2 pt-2 px-2">
+      {/* <div className="max-w-[1440px] mx-auto lg:p-20 pb-2 pt-2 px-2">
         <div className="lg:p-20 lg:rounded-3xl bg-gray-100 py-16">
           <h2 className="mb-3 font-semibold lg:text-[48px] text-3xl text-center lg:text-left">Featured Property</h2>
           <p className="text-center lg:text-left text-gray-400 sm:mb-12 mb-10">Let&apos;s find you a comfortable place</p>
-
           <h3 className="text-center lg:text-left text-3xl font-semibold mb-8">Rent</h3>
           <p className="text-center lg:text-left text-gray-400 sm:mb-12 mb-10">Let&apos;s find you a comfortable place</p>
-
+          <img src={require("../components/featured/rent.jpeg")} alt="buy" className="rounded-xl" />
           <h3 className="text-center lg:text-left text-3xl font-semibold mb-8">Buy</h3>
+          <img src={require("../components/featured/buy.jpeg")} alt="buy" className="rounded-xl" />
+
           <p className="text-center lg:text-left text-gray-400 sm:mb-12 mb-10">Let&apos;s find you a comfortable place</p>
+          <img src={require("../components/featured/sell.jpeg")} alt="sell" />
+        </div>
+      </div> */}
+      <div className="max-w-[1440px] mx-auto lg:p-20 pb-2 pt-2 px-2">
+        <div className="lg:p-20 lg:rounded-3xl bg-gray-100 py-16">
+          <h2 className="mb-3 font-semibold lg:text-[48px] text-3xl text-center lg:text-left">Featured Property</h2>
+          <p className="text-center lg:text-left text-gray-400 sm:mb-12 mb-10">Let's find you a comfortable place</p>
+
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start">
+            <div className="flex flex-col items-center lg:items-start lg:mr-12">
+              <div className="image-container">
+                <img src={require("../components/featured/rent.jpeg")} alt="rent" className="image rounded-xl" />
+              </div>
+              <div className="image-content">
+                <h3 className="text-center lg:text-left text-3xl font-semibold mb-2">Rent</h3>
+                <p className="text-center lg:text-left text-gray-400">
+                  We’re creating a seamless online experience – from shopping on the largest rental network, to applying, to paying rent.
+                </p>
+                <a href="#" className="btn btn-rent">
+                  See Rentals
+                </a>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center lg:items-start lg:mr-12">
+              <div className="image-container">
+                <img src={require("../components/featured/buy.jpeg")} alt="buy" className="image rounded-xl" />
+              </div>
+              <div className="image-content">
+                <h3 className="text-center lg:text-left text-3xl font-semibold mb-2">Buy</h3>
+                <p className="text-center lg:text-left text-gray-400">
+                  Find your place with an immersive photo expirience and the most listings, including things you won't find elsewhere
+                </p>
+                <a href="#" className="btn btn-buy">
+                  See Options
+                </a>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center lg:items-start">
+              <div className="image-container">
+                <img src={require("../components/featured/sell.jpeg")} alt="sell" className="image rounded-xl" />
+              </div>
+              <div className="image-content">
+                <h3 className="text-center lg:text-left text-3xl font-semibold mb-2">Sell</h3>
+                <p className="text-center lg:text-left text-gray-400">
+                  No matter what path you take to sell your home, we can help you navigate a successful sale and with the best practice and safety in mind
+                </p>
+                <a href="#" className="btn btn-sell">
+                  Sell a Home
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- houzez/src/components/header.jsx
+++ houzez/src/components/header.jsx
@@ -46,7 +46,7 @@ export default function Header() {
   };
 
   return (
-    <div className="bg-white border-b shadow-xl sticky top-0 z-40 px-1">
+    <div className="bg-cyan-700 border-b shadow-xl sticky top-0 z-40 px-1">
       <header className="flex justify-between items-center px-2 max-w-7xl mx-auto">
         <div className="flex items-center">
           <img
@@ -72,7 +72,7 @@ export default function Header() {
 
         {isMobile ? (
           <div className="flex items-center">
-            <button className="p-2 rounded-md text-gray-600 hover:bg-gray-200 focus:outline-none focus:bg-gray-200" onClick={toggleMenu}>
+            <button className="p-2 rounded-md text-black hover:bg-cyan-700 focus:outline-none focus:bg-cyan-700" onClick={toggleMenu}>
               <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
@@ -81,7 +81,7 @@ export default function Header() {
         ) : (
           <ul className="flex space-x-10 my-5">
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold ${getPath("/") ? "border-b border-red-500 text-black" : "border-b-transparent text-gray-500"}`}
+              className={`cursor-pointer py-3 text-sm font-semibold ${getPath("/") ? "border-b border-red-500 text-black" : "border-b-transparent text-white"}`}
               onClick={() => {
                 closeMenu();
                 navigate("/");
@@ -90,7 +90,7 @@ export default function Header() {
               Home
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold ${getPath("/AllListings") ? "border-b border-red-500 text-black" : "border-b-transparent text-gray-500"}`}
+              className={`cursor-pointer py-3 text-sm font-semibold ${getPath("/AllListings") ? "border-b border-red-500 text-black" : "border-b-transparent text-white"}`}
               onClick={() => {
                 closeMenu();
                 navigate("/AllListings");
@@ -99,7 +99,7 @@ export default function Header() {
               Listings
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold ${getPath("/offers") ? "border-b border-red-500 text-black" : "border-b-transparent text-gray-500"}`}
+              className={`cursor-pointer py-3 text-sm font-semibold ${getPath("/offers") ? "border-b border-red-500 text-black" : "border-b-transparent text-white"}`}
               onClick={() => {
                 closeMenu();
                 navigate("/offers");
@@ -109,7 +109,7 @@ export default function Header() {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold ${
-                (getPath("/Login") || getPath("/profile")) && viewstate !== "Sign in" ? "border-b border-red-500 text-black" : "border-b-transparent text-gray-500"
+                (getPath("/Login") || getPath("/profile")) && viewstate !== "Sign in" ? "border-b border-red-500 text-black" : "border-b-transparent text-white"
               }`}
               onClick={() => {
                 closeMenu();
@@ -119,7 +119,7 @@ export default function Header() {
               {viewstate}
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold ${getPath("/Contacts") ? "border-b border-red-500 text-black" : "border-b-transparent text-gray-500"}`}
+              className={`cursor-pointer py-3 text-sm font-semibold ${getPath("/Contacts") ? "border-b border-red-500 text-black" : "border-b-transparent text-white"}`}
               onClick={() => {
                 closeMenu();
                 navigate("/Contacts");
@@ -132,9 +132,9 @@ export default function Header() {
       </header>
 
       {isMobile && isMenuOpen && (
-        <ul className="bg-white border-b border-gray-200 py-3 px-6 space-y-2">
+        <ul className="bg-cyan-700 border-b border-cyan-200 py-3 px-6 space-y-2">
           <li
-            className={`cursor-pointer text-sm font-semibold ${getPath("/") ? "text-red-500 underline font-bold" : "text-gray-500"}`}
+            className={`cursor-pointer text-sm font-semibold ${getPath("/") ? "text-red-500 underline font-bold" : "text-white "}`}
             onClick={() => {
               closeMenu();
               navigate("/");
@@ -143,7 +143,7 @@ export default function Header() {
             Home
           </li>
           <li
-            className={`cursor-pointer text-sm font-semibold ${getPath("/AllListings") ? "text-red-500 underline font-bold" : "text-gray-500"}`}
+            className={`cursor-pointer text-sm font-semibold ${getPath("/AllListings") ? "text-red-500 underline font-bold" : "text-white "}`}
             onClick={() => {
               closeMenu();
               navigate("/AllListings");
@@ -152,7 +152,7 @@ export default function Header() {
             Listings
           </li>
           <li
-            className={`cursor-pointer text-sm font-semibold ${getPath("/offers") ? "text-red-500 underline font-bold" : "text-gray-500"}`}
+            className={`cursor-pointer text-sm font-semibold ${getPath("/offers") ? "text-red-500 underline font-bold" : "text-white"}`}
             onClick={() => {
               closeMenu();
               navigate("/offers");
@@ -162,7 +162,7 @@ export default function Header() {
           </li>
           <li
             className={`cursor-pointer text-sm font-semibold ${
-              (getPath("/Login") || getPath("/profile")) && viewstate !== "Sign in" ? "font-bold text-red-500 underline " : "text-gray-500"
+              (getPath("/Login") || getPath("/profile")) && viewstate !== "Sign in" ? "font-bold text-red-500 underline " : "text-white"
             }`}
             onClick={() => {
               closeMenu();
@@ -172,7 +172,7 @@ export default function Header() {
             {viewstate}
           </li>
           <li
-            className={`cursor-pointer text-sm font-semibold ${getPath("/Contacts") ? "text-red-500 underline font-bold" : "text-gray-500"}`}
+            className={`cursor-pointer text-sm font-semibold ${getPath("/Contacts") ? "text-red-500 underline font-bold" : "text-white"}`}
             onClick={() => {
               closeMenu();
               navigate("/Contacts");
